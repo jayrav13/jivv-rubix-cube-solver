3abf295c36a9cc2c2d13f6e68839bb8026b4f960
jayrav13 <jayrav13@gmail.com>
2015-09-26 14:27:28 -0400

    Initial Commit

---
+++ JIVV-Rubix-Cube-Solver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JIVV-Rubix-Cube-Solver
+//
+//  Created by Jay Ravaliya on 9/26/15.
+//  Copyright © 2015 JRav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
